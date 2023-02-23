6d0d6f938c3bb2f7e2c33e07b97255b0407e19fa
lclas <lclas@BOOK-8OSULRDKVT>
2023-02-23 15:14:14 +0900

    네비게이션바 로고 크기 수정, 공지사항 리스트 및 글쓰기 구현, mybatis mapper xml 설정

--- springpj/src/main/java/com/springpj/teampj/controller/NoticeController.java
+++ springpj/src/main/java/com/springpj/teampj/controller/NoticeController.java
@@ -1,12 +1,22 @@
 package com.springpj.teampj.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.springpj.teampj.HomeController;
+import com.springpj.teampj.model.NoticeDTO;
 import com.springpj.teampj.service.NoticeService;
+import com.sun.media.jfxmedia.logging.Logger;
 
 @Controller
 public class NoticeController {
@@ -14,11 +24,41 @@ public class NoticeController {
 	@Autowired
 	NoticeService noticeServiceImpl;
 	
+	//공지사항 전체글 리스트
 	@RequestMapping(value="notice", method=RequestMethod.GET)
-	public String getNoticeList(Model model) {
-		int result = noticeServiceImpl.getNoticeList();
-		model.addAttribute("result",result);
+	public String getNoticeList(Model model, HttpServletRequest req) {
+		
+		//세션
+//		HttpSession session = req.getSession();
+//		String id = (String) session.getAttribute("id");
+//		System.out.println("id값:"+id);
+		
+		
+		List<NoticeDTO> noticeList = noticeServiceImpl.getNoticeList();
+		
+		model.addAttribute("noticeList",noticeList);
 		return "notice/noticeList";
 	}
+	
+	//공지사항 글쓰기 폼 보여주기
+	@RequestMapping(value="notice/insert", method=RequestMethod.GET)
+	public String insertNoticeForm() {
+		
+		return "notice/insertForm";
+	}
+	
+	@RequestMapping(value="notice/insert", method=RequestMethod.POST)
+	public String insertNotice(NoticeDTO noticeDTO) {
+		
+		System.out.println("noticeDTO="+noticeDTO.toString());
+		int cnt = noticeServiceImpl.noticeInsert(noticeDTO);
+		if(cnt == 0) {
+			System.out.println("공지사항 글쓰기 실패");
+			return null;
+		}
+		return "redirect:/notice";
+	}
+	
+	
 	
 }
--- springpj/src/main/java/com/springpj/teampj/service/NoticeService.java
+++ springpj/src/main/java/com/springpj/teampj/service/NoticeService.java
@@ -1,11 +1,14 @@
 package com.springpj.teampj.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
 import com.springpj.teampj.model.NoticeDTO;
 
 public interface NoticeService {
 	
 	//공지사항 게시글 리스트
-	public int getNoticeList();
+	public List<NoticeDTO> getNoticeList();
+	
+	//공지사항 글작성
+	public int noticeInsert(NoticeDTO noticeDTO);
 }
